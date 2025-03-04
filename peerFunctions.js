6dbcb9fb577db5076ec9425fe659574cc4d303ba
Jones Yu <jonesyu@Joness-MacBook-Air.local>
2025-03-04 16:19:25 +0800

    added simpler id for easy input

--- peerFunctions.js
+++ peerFunctions.js
@@ -2,7 +2,11 @@ var ID;
 
 async function createPeer() {
     return new Promise((resolve, reject) => {
-        const peer = new Peer();
+        // generate unique id with excatly 10 upper english letters
+        var id = Math.random().toString(36).substring(2, 9).toUpperCase();
+
+        //create peer instance
+        const peer = new Peer(id);
         peer.on('open', function(id) {
             console.log('My peer ID is: ' + id);
             ID = id;
