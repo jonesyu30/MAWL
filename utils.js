d2a069adb3dc63380a49d1994d6d1408f04d67cf
Jones Yu <jonesyu@Joness-MacBook-Air.local>
2025-03-04 15:07:09 +0800

    added choose room interface

--- utils.js
+++ utils.js
@@ -1,6 +1,21 @@
 function changeScreen(fromDiv, toDiv) {
+    // Hide the current screen
     var screen = document.getElementById(fromDiv);
     screen.style.display = "none";
+    // Show the new screen
     screen = document.getElementById(toDiv);
     screen.style.display = "block";
+}
+
+var NAME;
+function getName(name){
+    var name = document.getElementById('name-input').value;
+    if(name == '') {
+        alert('Please enter your name');
+        return false;
+    } else {
+        NAME = name;
+        document.getElementById("name-input").value = "";
+        return true;
+    }
 }
